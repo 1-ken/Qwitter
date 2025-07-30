31fa2579ec47c6772e2122511f58b341b1dbdcb8
1-ken <kennicmwimu@gmail.com>
2025-07-30 18:48:28 +0300

    refactor: update layout styles and enhance drawer functionality

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,23 +1,40 @@
 <template>
   <q-layout view="lHr lpR fFf">
-
-    <q-header bordered class="bg-primary text-white">
+    <q-header bordered class="bg-white text-black">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          Title
-        </q-toolbar-title>
-
-        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+        <q-toolbar-title class="text-weight-bold"> Qwitter </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
-      <!-- drawer content -->
+    <q-drawer :width="283" show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <!-- left drawer content -->
+      <q-icon
+        name="fa-solid fa-dove"
+        size="lg"
+        color="primary"
+        class="q-pa-md"
+      />
+      <q-list>
+        <q-item to="/" clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon size="md"  name="home" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+        </q-item>
+      </q-list>
+      <q-list>
+        <q-item to="/about" clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon size="md"  name="help" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold">About</q-item-section>
+        </q-item>
+      </q-list>
+
     </q-drawer>
 
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
@@ -27,29 +44,28 @@
     <q-page-container>
       <router-view />
     </q-page-container>
-
   </q-layout>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 export default {
-  setup () {
-    const leftDrawerOpen = ref(false)
-    const rightDrawerOpen = ref(false)
+  setup() {
+    const leftDrawerOpen = ref(false);
+    const rightDrawerOpen = ref(false);
 
     return {
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
       },
 
       rightDrawerOpen,
-      toggleRightDrawer () {
-        rightDrawerOpen.value = !rightDrawerOpen.value
-      }
-    }
-  }
-}
+      toggleRightDrawer() {
+        rightDrawerOpen.value = !rightDrawerOpen.value;
+      },
+    };
+  },
+};
 </script>
