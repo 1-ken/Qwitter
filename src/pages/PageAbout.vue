3b1d7a0443086135449589de0f7929af5cb774f6
1-ken <kennicmwimu@gmail.com>
2025-07-30 21:49:56 +0300

    feat: enhance PageAbout component with improved layout and styling

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,12 +1,25 @@
 <template>
-  <q-page >
-    <h5>About</h5>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h4>
+    <div class="text-body1">
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia est
+        voluptatem deleniti magnam nostrum ut dignissimos deserunt blanditiis
+        facilis, alias iste labore, ab odio aut cumque quis aliquam
+        necessitatibus magni!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia est
+        voluptatem deleniti magnam nostrum ut dignissimos deserunt blanditiis
+        facilis, alias iste labore, ab odio aut cumque quis aliquam
+        necessitatibus magni!
+      </p>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
   name: "PageAbout",
-
 };
 </script>
